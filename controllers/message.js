d194ec91981a9a1cae69108f6ceb70c535d52ef3
Ryan Scarbel <ryanscarbel@gmail.com>
2021-10-26 14:27:58 -0400

    Added get route for retreiving messages

--- controllers/message.js
+++ controllers/message.js
@@ -2,4 +2,18 @@ const express = require('express');
 const router = express.Router();
 const Message = require('../models/message');
 
+router.get('/messages', async (req, res) => {
+  try {
+      const messages = await Message.find({});
+      res.json(messages);
+  } catch (error) {
+      console.log(error);
+      res.status(500).json('Your request could not be completed');
+  }
+});
+
+router.get('*', (req,res) => {
+    res.status(500).json('Please make a valid request.')
+})
+
 module.exports = router;
