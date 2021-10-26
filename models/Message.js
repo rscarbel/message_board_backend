d7ac884443f78e69421319a9d9b9c438d5183707
Ryan Scarbel <ryanscarbel@gmail.com>
2021-10-26 14:31:43 -0400

    Made username and message required

--- models/Message.js
+++ models/Message.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const messageSchema = new Schema({
-  username: String,
-  message: String
+  username: {type: String, required: true},
+  message: {type: String, unique: true, required: true}
 },{timestamps: true})
 
 module.exports = mongoose.model('message', messageSchema)
