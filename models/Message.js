b37d4e04431ad446569a848af56e96544c461369
Ryan Scarbel <ryanscarbel@gmail.com>
2021-10-27 13:25:26 -0400

    Added ip address & limited length of inputs

--- models/Message.js
+++ models/Message.js
@@ -2,8 +2,19 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const messageSchema = new Schema({
-  username: {type: String, default: 'anonymous'},
-  message: {type: String, unique: true, required: true}
+  username: {type: String,
+    default: 'anonymous',
+    min: 1,
+    max: 30
+  },
+  message: {
+    type: String,
+    unique: true,
+    required: true,
+    min: 1,
+    max: 500
+  },
+  IP: String
 },{timestamps: true})
 
 module.exports = mongoose.model('message', messageSchema)
